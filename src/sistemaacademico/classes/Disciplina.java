e248bdb49e482a4ee98857e29dc1cd4bbdf0e531
viniciusbj <vini09brito@hotmail.com>
2019-10-01 19:56:57 -0300

    adptando ao UML da entrega0

--- src/sistemaacademico/classes/Disciplina.java
+++ src/sistemaacademico/classes/Disciplina.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemaacademico.classes;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author guest-ychbik
- */
 public class Disciplina {
     private String nome;
     private String codigo;
     private int cargaHoraria;
-    private ArrayList<Disciplina> preRequisitos;
 
     public Disciplina(String nome, String código, int cargaHoraria) {
         this.nome = nome;
