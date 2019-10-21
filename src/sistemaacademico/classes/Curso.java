ab6a43afb91719b724030c6f6e645c153e7aaefc
danielevalverde <danierzevalverde@gmail.com>
2019-10-21 07:15:47 -0300

    fix classCurso

--- src/sistemaacademico/classes/Curso.java
+++ src/sistemaacademico/classes/Curso.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 
 
 public class Curso {
-    private String nome;   
+    private String nome; 
+    private ArrayList<DisciplinaCurso> disciplinas;
 
     public Curso(String nome) {
         this.nome = nome;
@@ -23,9 +24,24 @@ public class Curso {
        this.nome = nome;
     }
     
-    public void imprimeCurriculo(DisciplinaCurso disciplina) {
-    	 
+    public void addDisciplinas(DisciplinaCurso disciplina) {
+    	this.disciplinas.add(disciplina);
+    	
+    }
+    
+    public void imprimeCurriculo() {
+    	for (int i = 0; i < disciplinas.size(); i++) {
+			System.out.println(disciplinas.get(i).getNome());
+		} 
 	}
+    
+    public void disciplinaSemetre(){
+    	for (int i = 0; i < disciplinas.size(); i++) {	
+    		if( disciplinas.get(i).getSemestre() == i ) {
+    			System.out.println(disciplinas.get(i).getNome());
+    		}
+		} 
+    }
     
     
 }
