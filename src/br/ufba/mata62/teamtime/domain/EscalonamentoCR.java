885b9479007689ded78f6a206149716b1db0e058
viniciusbj <vini09brito@hotmail.com>
2019-11-19 15:02:45 -0300

    wip comparator

--- src/br/ufba/mata62/teamtime/domain/EscalonamentoCR.java
+++ src/br/ufba/mata62/teamtime/domain/EscalonamentoCR.java
@@ -1,12 +1,19 @@
 package br.ufba.mata62.teamtime.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class EscalonamentoCR implements CriterioEscalonamento {
+public class EscalonamentoCR implements CriterioEscalonamento, Comparator<Aluno> {
 
 	@Override
 	public ArrayList<Aluno> escalonaAlunos(ArrayList<Aluno> alunos) {
-		return alunos;
+		return Collections.sort(alunos, new EscalonamentoCR());
+	}
+
+	@Override
+	public int compare(Aluno a, Aluno b) {
+		return a.getCR()-b.getCR();
 	}
 
 }
